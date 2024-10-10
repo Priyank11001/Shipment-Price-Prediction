d169b0a5b377f592184642d9b2fa1f4a89ac381f
Priyank <982005priyank@gmail.com>
2024-10-10 19:22:24 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="Shipment",
+    version="0.0.1",
+    author="Priyank Zezariya",
+    author_email="982005priyank@gmail.com",
+    packages=find_packages(),
+    install_requires = []
+    )
